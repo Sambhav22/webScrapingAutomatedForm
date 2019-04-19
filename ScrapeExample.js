7c92f0ff0146a8163c6f2f1dcac5643da8d08f20
Sambhav Jain <jainsmbhv22@gmail.com>
2019-04-20 01:15:43 +0530

    Revert "first commit"
    This reverts commit 19306591999a74e3d1bc305dcfc95895df43683c.

--- ScrapeExample.js
+++ ScrapeExample.js
@@ -1,23 +1,49 @@
 var webdriver = require("selenium-webdriver"),
   By = webdriver.By;
-var chrome = require("chromedriver");
-var fs = require("fs");
 var driver = new webdriver.Builder().forBrowser("chrome").build();
 
-driver.get("https://www.google.com/");
+driver.get("file://" + __dirname + "/index.html");
 
-pause(2, ScreenShot);
+pause(2, ScrapeExample);
 
-function ScreenShot() {
-  console.log("ScreenShoting the page");
-  driver.takeScreenshot().then(function(image, err) {
-    fs.writeFile("./images/google.png", image, "base64", function(err) {
-      console.log(err);
-      if (err == null) {
-        console.log("Screenshot has been taken");
-      }
+function ScrapeExample() {
+  console.log("Scrapping the page");
+
+  driver
+    .findElement(By.id("name"))
+    .sendKeys("Sambhav Jain")
+    .then(null, function(exception) {
+      console.log(exception.name);
     });
-  });
+
+  driver
+    .findElement(By.xpath('//input[@value="other"]'))
+    .click()
+    .then(null, function(exception) {
+      console.log(exception.name);
+    });
+
+  driver
+    .findElement(By.xpath('//input[@type="checkbox"]'))
+    .click()
+    .then(null, function(exception) {
+      console.log(exception.name);
+    });
+
+  driver
+    .findElements(By.id("fruits"))
+    .then(function(value) {
+      for (var i = 0; i < value.length; i++) {
+        value[i].getText().then(function(text) {
+          console.log(text);
+        });
+      }
+    })
+    .then(null, function(exception) {
+      console.log(exception.name);
+    });
+
+  driver.findElement(By.xpath('//option[@value="BMW"]')).click();
 
   pause(7, QuitDriver);
 }
